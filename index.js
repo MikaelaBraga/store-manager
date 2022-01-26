c1455ca58c387a28adb40bba52374b8c15813fa9
MIkaela Braga <bragamikaela@gmail.com>
2022-01-25 23:25:20 -0300

    Camadas de model, service e controllers estruturaradas

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 require('dotenv').config();
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const productController = require('./controllers/productController');
+const middelware = require('./middlewares/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/products', productController);
+
+app.use(middelware.joiError);
+app.use(middelware.domainError);
+app.use(middelware.internalError);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
