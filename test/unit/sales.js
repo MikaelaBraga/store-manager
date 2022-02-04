655b11bf7dab6bc7c3253f30b887ae8c7338caff
MIkaela Braga <bragamikaela@gmail.com>
2022-02-04 12:24:16 -0300

    Iniciando testes da camada Service

---
+++ test/unit/sales.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../models/connection');
+const productsService = require('../../services/productService');
+
+describe('TESTE DA CAMADA SERVICE', () => {
+
+  describe('Teste da camada Service Products', () => {
+
+    describe('Teste da função registerProducts', () => {});
+
+  });
+
+});
